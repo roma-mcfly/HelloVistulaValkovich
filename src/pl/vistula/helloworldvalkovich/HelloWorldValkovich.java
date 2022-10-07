0c04393582294646f280e26b019d881d94cc301c
Roman <58719267+roma-mcfly@users.noreply.github.com>
2022-10-07 14:24:56 +0200

    v2

--- src/pl/vistula/helloworldvalkovich/HelloWorldValkovich.java
+++ src/pl/vistula/helloworldvalkovich/HelloWorldValkovich.java
@@ -5,7 +5,7 @@ public class HelloWorldValkovich {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
 
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld1");
     }
 }
 
